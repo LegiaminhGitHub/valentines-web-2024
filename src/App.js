2b13efccd7635c373be56c5867abb96d3ca0f76c
LegiaminhGitHud <legiaminhoffice@gmail.com>
2024-02-16 12:22:10 +0700

    web analytics update

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
 import Main_page from "./main-page";
 import Thank_you from './thank_you';
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Main_page/>}></Route>
-        <Route path="/thanks" element={<Thank_you/>}></Route>
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main_page/>}></Route>
+          <Route path="/thanks" element={<Thank_you/>}></Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
